0b14ee08be0e20d29130f97fda20547e8f7e3402
anndavpet <31045303+anndavpet@users.noreply.github.com>
2022-07-24 20:09:41 +0300

    Create 7_11_1.py

---
+++ 7_11_1.py
@@ -0,0 +1,11 @@
+def show_menu(func):
+    def wrapper(s):
+        res = func(s)
+        for key, value in enumerate(res):  
+            print(f"{key+1}. {value}")
+    return wrapper
+
+
+@show_menu
+def get_menu(s):
+    return s.split()
