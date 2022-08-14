d07c9115cebaec9e45109d78ed2c4c3c1511d793
anndavpet <31045303+anndavpet@users.noreply.github.com>
2022-08-14 11:56:52 +0300

    a

--- ex1.py
+++ ex1.py
@@ -1,6 +1,25 @@
-import courses
-import pprint
+def is_simple(x):
+    d = x -1
+    if d < 0:
+        return False
 
-print(courses.python_doc.doc)
-courses.get_php()
-pprint.pprint(dir(courses))
+    while d > 1:
+        if x % d == 0:
+            return False
+        d -= 1
+
+    return True
+
+
+a = [x for x in range(1, 11)]
+
+b = filter(is_simple, a)
+
+for i in b:
+    print(i)
+
+lst = ['Virginia', 'Florida', 'Trafalgar    ', 'Moscow']
+
+b = filter(str.isalpha, lst)
+
+print(tuple(b))
--- main.py
+++ main.py
@@ -1,23 +1,2 @@
-# function read string
-# decorator with parameters
-# argument 'chars' in decorator
-
-def df_decorator():
-    def func_decorator(func):
-        def wrapper(*args, **kwargs):
-            res = func(*args, **kwargs)
-
-            return res
-        return wrapper
-    return func_decorator
-
-
-def get_list(*args):
-    return args
-
-
-s = input()
-
-f = get_list(s)
-
-print(f)
+import sys
+sys.stdout.write('HELLO PYTHON')
